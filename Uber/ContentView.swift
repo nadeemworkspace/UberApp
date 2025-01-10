d438f5cd1b8a63ad9120ea9df63622ba99dc07fd
Muhammed Nadeem <muhammednadeem989@gmail.com>
2025-01-10 22:02:11 +0530

    Initial Commit

---
+++ Uber/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Uber
+//
+//  Created by Muhammed Nadeem M A on 10/01/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Uber/UberApp.swift
@@ -0,0 +1,17 @@
+//
+//  UberApp.swift
+//  Uber
+//
+//  Created by Muhammed Nadeem M A on 10/01/25.
+//
+
+import SwiftUI
+
+@main
+struct UberApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
