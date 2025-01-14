ed5cb94f1352210fbd3ffeec8b6661bcd8b4db2a
Muhammed Nadeem <muhammednadeem989@gmail.com>
2025-01-14 19:54:04 +0530

    added map and user location functionality

--- Uber/Core/Home/View/MapViewRepresentable.swift
+++ Uber/Core/Home/View/MapViewRepresentable.swift
@@ -11,8 +11,13 @@ import MapKit
 struct MapViewRepresentable: UIViewRepresentable{
     
     let mapView = MKMapView()
+    private let locationManager = LocationManager()
 
     func makeUIView(context: Context) -> some UIView {
+        mapView.delegate = context.coordinator
+        mapView.isRotateEnabled = false
+        mapView.showsUserLocation = true
+        mapView.userTrackingMode = .follow
         return mapView
     }
     
@@ -38,6 +43,14 @@ extension MapViewRepresentable {
             super.init()
         }
 
+        func mapView(_ mapView: MKMapView, didUpdate userLocation: MKUserLocation) {
+            let region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: userLocation.coordinate.latitude,
+                                                                           longitude: userLocation.coordinate.longitude),
+                                            span: MKCoordinateSpan(latitudeDelta: 0.05,
+                                                                   longitudeDelta: 0.05))
+            parent.mapView.setRegion(region, animated: true)
+        }
+        
     }
     
 }
---
+++ Uber/Manager/LocationManager.swift
@@ -0,0 +1,31 @@
+//
+//  LocationManager.swift
+//  Uber
+//
+//  Created by Muhammed Nadeem M A on 14/01/25.
+//
+
+import CoreLocation
+
+class LocationManager: NSObject, ObservableObject{
+    
+    private let locationManager = CLLocationManager()
+    
+    override init() {
+        super.init()
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+    }
+    
+}
+
+extension LocationManager: CLLocationManagerDelegate {
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard !locations.isEmpty else { return }
+        locationManager.stopUpdatingLocation()
+    }
+    
+}
